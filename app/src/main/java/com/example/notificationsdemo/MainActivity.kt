485f5e15852e950bc6a0b0808f3bd164d624b790
Keshav Pai <keshu.pai@gmail.com>
2025-04-21 11:41:20 +0530

    Tap action on the Notification

--- app/src/main/java/com/example/notificationsdemo/MainActivity.kt
+++ app/src/main/java/com/example/notificationsdemo/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.notificationsdemo
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -44,12 +46,19 @@ class MainActivity : AppCompatActivity() {
     // Displays an Icon, Title and Description
     private fun displayNotification() {
         val notificationId = 45
+
+        val tapResultIntent = Intent(this, SecondActivity::class.java)
+        val pendingIntent : PendingIntent = PendingIntent.getActivity(
+            this, 0, tapResultIntent, PendingIntent.FLAG_MUTABLE
+        )
+
         val notification = NotificationCompat.Builder(this@MainActivity, channelID)
             .setContentTitle("Demo Title")
             .setContentText("This is Demo Notification")
             .setSmallIcon(android.R.drawable.ic_dialog_info)
             .setAutoCancel(true)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
             .build()
         notificationManager?.notify(notificationId, notification)
     }
